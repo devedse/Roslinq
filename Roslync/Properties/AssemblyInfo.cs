accd4f3737e938c3e075739fa73b25f28539cb81
Devedse <davydavidse@gmail.com>
2016-05-20 23:23:11 +0200

    Added nuspec stuff

--- Roslync/Properties/AssemblyInfo.cs
+++ Roslync/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Roslync")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("With Roslync it's possible to use Lync to browse through source code and modify it.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Devedse's cool company")]
 [assembly: AssemblyProduct("Roslync")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
